fea8018c7e257d70e057a4392e5e59f2f0703fe6
LandonPatam <lcpatam@gmail.com>
2025-12-05 21:15:32 -0800

    added keybind for resting game and for solving using AI

--- input_handler.py
+++ input_handler.py
@@ -11,14 +11,33 @@ def handle_input(sokoban_state):
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
-
+            
+            # UP ARROW KEY
             if event.key == pygame.K_UP:
                 sokoban_state.move_player((0, -1))
+                return "move"
+            
+            # DOWN ARROW KEY
             elif event.key == pygame.K_DOWN:
                 sokoban_state.move_player((0, 1))
+                return "move"
+            
+            # LEFT ARROW KEY
             elif event.key == pygame.K_LEFT:
                 sokoban_state.move_player((-1, 0))
+                return "move"
+            
+            # RIGHT ARROW KEY
             elif event.key == pygame.K_RIGHT:
                 sokoban_state.move_player((1, 0))
+                return "move"
+
+            # R KEY RESETS GAME
+            if event.key == pygame.K_r:
+                return "reset"
+            
+            # S KEY SOLVES GAME USING AI
+            if event.key == pygame.K_s:
+                return "solve_using_AI"
 
     return None
