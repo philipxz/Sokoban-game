a14a7184a0d72f74c7c9f766234e91ae7cb5b49f
LandonPatam <lcpatam@gmail.com>
2025-12-05 21:18:07 -0800

    A* solver implementation

---
+++ A_STAR_game_solver.py
@@ -0,0 +1,76 @@
+import heapq
+
+POSSIBLE_MOVES = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+
+
+# USES SUM OF MANHATTAN DISTANCES FROM EVERY BOX TO THE CLOEST TARGET POSITION
+def heuristic(state):
+    total_dist = 0
+    for box in state.boxes:
+        min_dist = float('inf')
+        for target in state.targets:
+            dist = abs(box[0] - target[0]) + abs(box[1] - target[1])
+            if dist < min_dist:
+                min_dist = dist
+        total_dist += min_dist
+    return total_dist
+
+
+# CHECKS IF A BOX IS STUCK IN A CORNER
+def is_deadlock(state):
+    for box in state.boxes:
+        if box in state.targets:
+            continue
+            
+        x, y = box
+        cant_move_up = (x, y-1) in state.walls
+        cant_move_down = (x, y+1) in state.walls
+        cant_move_left = (x-1, y) in state.walls
+        cant_move_right = (x+1, y) in state.walls
+        
+
+        if (cant_move_up and cant_move_left) or (cant_move_up and cant_move_right) or (cant_move_down and cant_move_left) or (cant_move_down and cant_move_right):
+            return True
+            
+    return False
+
+def solve_using_A_STAR(starting_board_layout):
+    
+    initial_board = starting_board_layout
+    start_snapshot = initial_board.get_snapshot_of_boxes_and_player()
+    
+    # PRIORITY QUEUE
+    AI_potential_game_states = []
+    tie_breaker = 0
+    heapq.heappush(AI_potential_game_states, (heuristic(initial_board), 0, tie_breaker, initial_board, []))
+    
+    AI_visited_snapshots  = set()
+    
+    
+    AI_visited_snapshots.add(start_snapshot)
+
+    while AI_potential_game_states:
+        total_cost, backward_cost, _, current_state, path = heapq.heappop(AI_potential_game_states)
+
+        if current_state.is_solved():
+            return path
+
+        for move in POSSIBLE_MOVES:
+            new_state = current_state.clone()
+            valid_move = new_state.move_player(move)
+
+            if valid_move:
+                
+                new_snapshot = new_state.get_snapshot_of_boxes_and_player()
+
+                if new_snapshot not in AI_visited_snapshots:
+                    if not is_deadlock(new_state):
+                        AI_visited_snapshots.add(new_snapshot)
+                        new_backward_cost = backward_cost + 1
+                        new_heuristic = heuristic(new_state)
+                        new_total_cost = new_backward_cost + new_heuristic
+                        
+                        tie_breaker += 1
+                        heapq.heappush(AI_potential_game_states, (new_total_cost, new_backward_cost, tie_breaker, new_state, path + [move]))
+    
+    return None
